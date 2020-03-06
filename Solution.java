358c51e120f038030ebf8003cd570dba985f3016
Rabin Gurung <48662958+rabinGurung@users.noreply.github.com>
2020-03-07 03:04:38 +0545

    cleaned some code

--- Solution.java
+++ Solution.java
@@ -6,19 +6,23 @@ class Solution {
     public static void main(String[] args){
         final int[] Dashboard_values = {Given_Number[0],Given_Number[1],Given_Number[2],Given_Number[3],Given_Number[1]};
         if(n == 0){
-            System.out.println("***************************************");
-            System.out.println("First Values");
+            for(int x = 0; x<25; x+=1){
+                System.out.print("*");
+            }
+            System.out.println("\n First Values");
             print_All(Dashboard_values);
-            System.out.println("***************************************");
+            for(int x = 0; x<25; x+=1){
+                System.out.print("*");
+            }
         }
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 if(n!= 0){
-                    System.out.println("At "+n*5+" seconds");
+                    System.out.println("\n At "+n*5+" seconds");
                     if(n > 23){
-                        System.out.println("Terminating System || 2 mins over");
+                        System.out.println(" \n Terminating System || 2 mins over");
                         timer.cancel();
                         timer.purge();
                         return;
@@ -30,7 +34,9 @@ class Solution {
                     Dashboard_values[4]+= 3;
                     print_All(Dashboard_values);
                     print_Alert_Values_IF_Exist(Dashboard_values);
-                    System.out.println("***************************************");    
+                    for(int x = 0; x<25; x+=1){
+                        System.out.print("*");
+                    }    
                     
                 }
                 n+=1;
@@ -41,14 +47,16 @@ class Solution {
     private static void print_Alert_Values_IF_Exist(int[] arr){
         for(int i = 0;i<arr.length; i++){
             if(arr[i] < 15 || arr[i] > 35){
-                System.out.println("Alert at value position "+arr[i]);
-                System.out.println("***************************************");    
+                System.out.println("\n Alert at value position "+arr[i]);
+                for(int x = 0; x<25; x+=1){
+                    System.out.print("*");
+                }    
             }
         }
     }
     private static void print_All(int[] arr){
         for(int i = 0;i<arr.length; i++){
-            System.out.println(arr[i]);
+            System.out.println("\n "+arr[i]);
         }
     }
 }
